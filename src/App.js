966fdcaf0d7ad176dbb4ab179eeea25e22055f9b
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-05-06 20:12:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -258,7 +258,7 @@ function App() {
               </StyledLink>
             </s.TextDescription>
             <s.SpacerSmall />
-            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+            {Number(1111-supply) >= CONFIG.MAX_SUPPLY ? (
               <>
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
